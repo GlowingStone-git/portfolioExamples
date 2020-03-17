c8ff79d9c80d4a052e7b66b69c107217c6152fd3
DaoistXiong <falens1@ya.ru>
2020-03-17 20:03:43 +0300

    waxom template

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// carousel init
+$('.carousel').carousel({
+    interval: false
+});
+
+//video init 
+$('#play').on('click', function (e) {
+    e.preventDefault();
+    $("#player")[0].src += "?autoplay=1";
+    $('#player').show();
+    $('#video-cover').hide();
+  })
+  $('#modal1').on('hidden.bs.modal', function (e) {
+    $('#modal1 iframe').attr("src", $("#modal1 iframe").attr("src"));
+  });
